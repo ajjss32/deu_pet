b2d3f390f59b84f4fc7e8177f46d4da9dfb65d9f
Ramon Alex <ramonalexf@gmail.com>
2025-03-30 16:46:11 -0300

    adicionado create and delete ao favoritos service para criacao de match

--- lib/services/favorito_service.dart
+++ lib/services/favorito_service.dart
@@ -3,26 +3,56 @@ import 'package:flutter/material.dart';
 import 'package:deu_pet/model/favorito.dart';
 
 class FavoritoService {
-  final CollectionReference favoritosCollection = FirebaseFirestore.instance.collection('favoritos');
+  final CollectionReference favoritosCollection =
+      FirebaseFirestore.instance.collection('favoritos');
 
-  Future<List<Favorito>> buscarFavoritosPorUsuario(String usuarioId, BuildContext context) async {
+  Future<List<Favorito>> buscarFavoritosPorUsuario(
+      String usuarioId, BuildContext context) async {
     try {
-      QuerySnapshot querySnapshot = await favoritosCollection.where('usuario_id', isEqualTo: usuarioId).get();
-      return querySnapshot.docs.map((doc) => Favorito.fromMap(doc.id, doc.data() as Map<String, dynamic>)).toList();
+      QuerySnapshot querySnapshot = await favoritosCollection
+          .where('usuario_id', isEqualTo: usuarioId)
+          .get();
+      return querySnapshot.docs
+          .map((doc) =>
+              Favorito.fromMap(doc.id, doc.data() as Map<String, dynamic>))
+          .toList();
     } catch (e) {
       print('Erro ao buscar favoritos: $e');
       return [];
     }
   }
 
-  Future<List<Favorito>> buscarFavoritosPorPet(String petId, BuildContext context) async {
+  Future<List<Favorito>> buscarFavoritosPorPet(
+      String petId, BuildContext context) async {
     try {
-      QuerySnapshot querySnapshot = await favoritosCollection.where('pet_id', isEqualTo: petId).get();
-      return querySnapshot.docs.map((doc) => Favorito.fromMap(doc.id, doc.data() as Map<String, dynamic>)).toList();
+      QuerySnapshot querySnapshot =
+          await favoritosCollection.where('pet_id', isEqualTo: petId).get();
+      return querySnapshot.docs
+          .map((doc) =>
+              Favorito.fromMap(doc.id, doc.data() as Map<String, dynamic>))
+          .toList();
     } catch (e) {
       print('Erro ao buscar favoritos: $e');
       return [];
     }
   }
 
+  Future<void> deletarFavorito(String id) async {
+    try {
+      await favoritosCollection.doc(id).delete();
+      print('Favorito deletado com sucesso!');
+    } catch (e) {
+      print('Erro ao deletar favorito: $e');
+    }
+  }
+
+  Future<void> criarFavorito(Favorito favorite) async {
+    try {
+      await favoritosCollection.doc(favorite.id).set(favorite.toMap());
+      
+      print('favorito criado com sucesso!');
+    } catch (e) {
+      print('Erro ao criar favorito: $e');
+    }
+  }
 }
--- lib/services/match_service.dart
+++ lib/services/match_service.dart
@@ -3,25 +3,69 @@ import 'package:flutter/material.dart';
 import 'package:deu_pet/model/match.dart';
 
 class MatchService {
-  final CollectionReference matchesCollection = FirebaseFirestore.instance.collection('matches');
+  final CollectionReference matchesCollection =
+      FirebaseFirestore.instance.collection('matches');
 
-  Future<List<Match>> buscarMatchesPorUsuario(String usuarioId, BuildContext context) async {
+  Future<List<Match>> buscarMatchesPorUsuario(
+    String usuarioId,
+    BuildContext context,
+  ) async {
     try {
-      QuerySnapshot querySnapshot = await matchesCollection.where('usuario_id', isEqualTo: usuarioId).get();
-      return querySnapshot.docs.map((doc) => Match.fromMap(doc.id, doc.data() as Map<String, dynamic>)).toList();
+      QuerySnapshot querySnapshot = await matchesCollection
+          .where('usuario_id', isEqualTo: usuarioId)
+          .get();
+      return querySnapshot.docs
+          .map((doc) =>
+              Match.fromMap(doc.id, doc.data() as Map<String, dynamic>))
+          .toList();
     } catch (e) {
       print('Erro ao buscar matches: $e');
       return [];
     }
   }
 
-  Future<List<Match>> buscarMatchesPorPet(String petId, BuildContext context) async {
+  Future<List<Match>> buscarMatchesPorPet(
+    String petId,
+    BuildContext context,
+  ) async {
     try {
-      QuerySnapshot querySnapshot = await matchesCollection.where('pet_id', isEqualTo: petId).get();
-      return querySnapshot.docs.map((doc) => Match.fromMap(doc.id, doc.data() as Map<String, dynamic>)).toList();
+      QuerySnapshot querySnapshot =
+          await matchesCollection.where('pet_id', isEqualTo: petId).get();
+      return querySnapshot.docs
+          .map((doc) =>
+              Match.fromMap(doc.id, doc.data() as Map<String, dynamic>))
+          .toList();
     } catch (e) {
       print('Erro ao buscar matches: $e');
       return [];
+    }
+  }
+
+  Future<void> criarMatch(Match match) async {
+    try {
+      await matchesCollection.doc(match.id).set(match.toMap());
+      print('match criado com sucesso!');
+    } catch (e) {
+      print('Erro ao criar match: $e');
+    }
+  }
+
+  Future<void> atualizarMatch(Match match) async {
+    try {
+      match.data = DateTime.now();
+      await matchesCollection.doc(match.id).update(match.toMap());
+      print('Match atualizado com sucesso!');
+    } catch (e) {
+      print('Erro ao atualizar match: $e');
+    }
+  }
+
+  Future<void> deletarMatch(String id) async {
+    try {
+      await matchesCollection.doc(id).delete();
+      print('Match deletado com sucesso!');
+    } catch (e) {
+      print('Erro ao deletar match: $e');
     }
   }
 }
